1078c74ce6db794ada8db07951142dda93913941
Martin Pöhlmann <martin.deimos@gmx.de>
2013-12-04 23:57:05 +0100

    Last commit before 2013/12/20

--- src/com/mpdeimos/winampscraper/Main.java
+++ src/com/mpdeimos/winampscraper/Main.java
@@ -1,12 +1,30 @@
 package com.mpdeimos.winampscraper;
 
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 /**
- * Main entry point for scraping winamp.com plugin, skin and visualization database.
- *  
+ * Main entry point for scraping winamp.com plugin, skin and visualization
+ * database.
+ * 
  * @author mpdeimos
  */
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Scraping started");
+	public static final String SAMPLE_URL = "http://www.winamp.com/plugin/details/222431";
+
+	public static void main(String[] args) throws ClientProtocolException,
+			IOException {
+
+		HttpClient client = HttpClients.createDefault();
+		HttpGet request = new HttpGet(SAMPLE_URL);
+		HttpResponse response = client.execute(request);
+
+		System.out.println(EntityUtils.toString(response.getEntity()));
 	}
 }
