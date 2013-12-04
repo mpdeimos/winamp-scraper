82bcf09885c1c6c637745a909e02f49130c76adb
Martin Pöhlmann <martin.deimos@gmx.de>
2013-12-04 23:31:37 +0100

    Initial structure including ivy and gson

---
+++ src/com/mpdeimos/winampscraper/Main.java
@@ -0,0 +1,12 @@
+package com.mpdeimos.winampscraper;
+
+/**
+ * Main entry point for scraping winamp.com plugin, skin and visualization database.
+ *  
+ * @author mpdeimos
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Scraping started");
+	}
+}
